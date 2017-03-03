4d3bd509b8d4ba5d110aaca05a75235eeab7872e
nwd3 <nwd3@students.calvin.edu>
2017-03-03 14:10:05 -0500

    nwd3 updated sesnwdShell
    Note commented execve command I think this is the best command to use.

--- sesnwdShell.h
+++ sesnwdShell.h
@@ -1,27 +1,36 @@
 /*
  * sesnwd3shell.h
  *
- *  Created on: Feb 27, 2017
- *      Author: nwd3
+ * Created on: Feb 27, 2017
+ * Author: nwd3
  */
-
+//http://stackoverflow.com/questions/10068327/what-does-execve-do
 #ifndef SESNWDSHELL_H_
 #define SESNWDSHELL_H_
 
 #include <iostream>
+#include <fstream>
+#include <iostream>
 #include "path.h"
-
+#include "commandline.h"
+#include "prompt.h"
 using namespace std;
 
-typedef double Item;
 class sesnwdShell {
 public:
-	sesnwdShell(){};
+	sesnwdShell() {
+	}
 	sesnwdShell(const sesnwdShell& original);
 	//~sesnwdShell();
 	void run() {
-		Path<string> p;
-		cout << "run"<<endl;
+		while (1) { //this works well we just cntrl c out
+			Prompt < string > prompt;
+			cout << prompt.get();
+			Path < string > p;
+			commandline<char> cmd;
+			cmd = commandline<char>(cin);
+			//execve(p.getpath(), cmd.getCommand(), p.pString);
+		}
 	}
 
 private:
