c27561f70af2b2e9c1b099f1967a77bf18e906b4
nwd3 <nwd3@students.calvin.edu>
2017-03-10 07:44:54 -0500

    now you can output multiple commands

--- sesnwdShell.h
+++ sesnwdShell.h
@@ -5,6 +5,7 @@
  * Author: nwd3
  */
 //http://stackoverflow.com/questions/10068327/what-does-execve-do
+//http://www.cs.ecu.edu/karl/4630/spr01/example1.html
 #ifndef SESNWDSHELL_H_
 #define SESNWDSHELL_H_
 //very important
@@ -17,7 +18,6 @@
 #include "commandline.h"
 #include "prompt.h"
 
-
 #include <errno.h>
 using namespace std;
 
@@ -27,23 +27,34 @@ public:
 	}
 	sesnwdShell(const sesnwdShell& original);
 	void run() {
+		commandline<char> cmd;
+
 		while (1) { //this works well we just cntrl c out
+			//	int ret = fork();
 			Prompt < string > prompt;
-			cout << prompt.get();
+			cout << prompt.get() << endl;
 			Path < string > p;
-			commandline<char> cmd;
+
 			cmd = commandline<char>(cin);
-			vector<string> arg;
+			vector < string > arg;
 			string com = cmd.getCommand();
 			//no segmentation fault
-			if(com=="exit")
-					break;
-			int index=p.find(com);
-			string path=p.getDirectory(index)+'/'+com;
-			execv(path.c_str(),cmd.getArgVector());
-			cout<<strerror(errno)<<endl;
+			if (com == "exit")
+				break;
+			pid_t child_pid;
+			int index = p.find(com);
+			string path = p.getDirectory(index) + '/' + com;
+
+			child_pid = fork();
+			int child_status;
+			if (child_pid == 0) {
+				execv(path.c_str(), cmd.getArgVector());
+			}
+
+			//cout << strerror(errno) << endl;
 			//execve(p.getPath[path], cmd.getCommand(), p.pString);
 		}
+		//run();
 	}
 
 private:
